a7872e8d8c0d38447486d5fea831dff8116b900c
Simin Hedayati <hedayati@gamelectronics.com>
2025-12-21 01:02:06 +0330

    first commit

---
+++ src/utils/list.ts
@@ -0,0 +1,32 @@
+export let drinks = [
+  {
+    name: "Cappuccino",
+    img: "/Cappuccino.png",
+    price: 3.5,
+    quantity: 0,
+  },
+  {
+    name: "Latte",
+    img: "/Latte.png",
+    price: 4,
+    quantity: 0,
+  },
+  {
+    name: "Espresso",
+    img: "/Espresso.png",
+    price: 2.5,
+    quantity: 0,
+  },
+  {
+    name: "Mocha",
+    img: "/Mocha.png",
+    price: 4.5,
+    quantity: 0,
+  },
+  {
+    name: "Americano",
+    img: "/Americano.png",
+    price: 3,
+    quantity: 0,
+  },
+];
